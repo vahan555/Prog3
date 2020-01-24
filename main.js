a17dc65a8ffb85fe1dd37e5a32dc387ea2d382c0
vahan555 <60002157+vahan555@users.noreply.github.com>
2020-01-24 16:57:56 +0400

    added files

---
+++ main.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req,res){
+    res.send("<h1>Hello world</h1>")
+})
+app.get("/name/:name", function(req,res){
+    res.send("<h1>Hello " + req.params.name + "</h1>")
+})
+
+app.listen(3001, function(){
+    console.log("Example is running on port 3000")
+})
